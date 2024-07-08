144df0c91d9cf066baa530e3343b31f8a858b7c2
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2024-07-08 10:41:05 -0300

    Ex2 Done

--- m1s7/src/app/app.routes.ts
+++ m1s7/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AlunosComponent } from './alunos/alunos.component';
+import { CadastroAlunosComponent } from './cadastro-alunos/cadastro-alunos.component';
+import { DisciplinasComponent } from './disciplinas/disciplinas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'alunos',
+        component: AlunosComponent
+    },
+    {
+        path: 'cadstro-alunos',
+        component: CadastroAlunosComponent
+    },
+    {
+        path: 'disciplinas',
+        component: DisciplinasComponent
+    }
+];
